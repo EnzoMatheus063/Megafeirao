8ecbc163472912e199655ed90bbdcb1a5c6bf3c7
EnzoMatheus063 <enzomatheus231@gmail.com>
2026-08-14 18:00:57 -0300

    14/08/2026

--- conexao/conexao.js
+++ conexao/conexao.js
@@ -3,10 +3,11 @@ const mysql= require("mysql2");
 
 // variável que armazena a conexão com o banco de dados
 const conexao = mysql.createConnection({
-    host: "localhost",
+    host: "hayabusa.proxy.rlwy.net",
     user: "root",
-    password: "",  // "senha do Mysql"
-    database: "MEGAFEIRAO"
+    port: 37622,
+    password: "bTCNPxLtpRzJgYNbRSFiYcgeURWedjMr",  // "senha do Mysql"
+    database: "railway"
 });
 conexao.connect((erro)=>{
     if(erro){
